6672d42df0b1661ba61a4a8d3e01180917c338d5
Pavel Yanushevsky <4erep.moroz@mail.ru>
2018-09-06 23:11:37 +0300

    Created class for working with file system

---
+++ includes/FileHelper.php
@@ -0,0 +1,62 @@
+<?php
+
+class FileHelper
+{
+    public static function saveImage($temp_path, $path)
+    {
+        move_uploaded_file($temp_path, $path);
+        chmod($path, 0644);
+    }
+
+    public static function checkFile($path)
+    {
+        $exts = ['image/jpeg', 'image/png'];
+        $real_ext = mime_content_type($path);
+
+        if (in_array($real_ext, $exts)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function getFileExt($type)
+    {
+        $ext = null;
+        switch ($type) {
+            case 'image/jpeg':
+                $ext = '.jpeg';
+                break;
+            case 'image/png':
+                $ext = '.png';
+                break;
+        }
+
+        return $ext;
+    }
+
+    public static function deleteFile($path)
+    {
+        if(file_exists($path)) {
+            unlink($path);
+        }
+    }
+
+    public static function deleteDirectory($dir_name)
+    {
+        $iterator = new RecursiveDirectoryIterator($dir_name);
+        $files = new RecursiveIteratorIterator(
+            $iterator, RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($files as $file) {
+            if (in_array($file->getFilename(), ['..', '.'])) {
+                continue;
+            }
+
+            ($file->isDir()) ? rmdir($file) : unlink($file);
+        }
+
+        rmdir($dir_name);
+    }
+}
