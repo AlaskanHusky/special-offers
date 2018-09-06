82b39b48987a088437d6ebcd0e3a5eb7e5de13d4
Pavel Yanushevsky <4erep.moroz@mail.ru>
2018-09-06 22:58:59 +0300

    Code refactoring, moved to classes

--- special-offers.php
+++ special-offers.php
@@ -2,9 +2,8 @@
 /**
  * Plugin Name:     Special Offers
  * Plugin URI:      https://github.com/AlaskanHusky/special-offers
- * Description:     Plugin "Special Offers" helps to add blocks with messages about special offers.
- *                  This can be done via shortcode.
- * Version:         1.0
+ * Description:     Plugin "Special Offers" helps to add blocks with messages about special offers. This can be done via shortcode.
+ * Version:         0.5
  * Author:          Pavel Yanushevsky
  * Author URI:      https://github.com/AlaskanHusky
  * License:         GPLv3
@@ -27,60 +26,8 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-register_activation_hook(__FILE__, 'specoff_plugin_activation');
-register_deactivation_hook(__FILE__, 'specoff_plugin_deactivation');
-// Adding plugin menu item
-add_action('admin_menu', 'specoff_admin_menu');
+require_once __DIR__ . '/includes/SpecialOffers.php';
 
-function specoff_plugin_activation()
-{
-    // Check Wordpress version
-    if (version_compare($GLOBALS['wp_version'], '3.1', '<')) {
-        wp_die('This plugin requires WordPress version 3.1 or higher.');
-    }
-
-    specoff_create_table();
-}
-
-function specoff_plugin_deactivation()
-{
-    remove_menu_page('specoff');
-}
-
-function specoff_create_table()
-{
-    global $wpdb;
-    $table_name = $wpdb->prefix . "so_offers";
-
-    //Failed to the database connection
-    if (!empty($wpdb->error)) {
-        wp_die($wpdb->error);
-    }
-
-    // Create plugin table
-    if ($wpdb->get_var("SHOW TABLES LIKE $table_name") != $table_name) {
-        $sql = "CREATE TABLE IF NOT EXISTS `$table_name` (
-	            `offer_id` INT(4) NOT NULL AUTO_INCREMENT,
-	            `offer_name` VARCHAR(100) NOT NULL,
-	            `offer_description` VARCHAR(500) NULL DEFAULT NULL,
-	            `offer_category` VARCHAR(100) NOT NULL,
-	            `offer_picture` VARCHAR(20) NOT NULL,
-	            `regular_price` FLOAT UNSIGNED NOT NULL,
-	            `offer_date` VARCHAR(16) NOT NULL,
-	            `special_price` FLOAT UNSIGNED NOT NULL,
-	            PRIMARY KEY (`offer_id`))
-                COLLATE='utf8_general_ci'
-                ENGINE=MyISAM;";
-        $wpdb->query($sql);
-    }
-}
-
-function specoff_admin_menu()
-{
-    add_menu_page('Special Offers', 'Special Offers', 8, 'specoff', 'specoff_plugin_page', 'dashicons-cart', "81.9");
-}
-
-function specoff_plugin_page()
-{
-
-}
+register_activation_hook(__FILE__, ['SpecialOffers', 'pluginActivation']);
+register_deactivation_hook(__FILE__, ['SpecialOffers', 'pluginDeactivation']);
+add_action('init', ['SpecialOffers', 'init']);
