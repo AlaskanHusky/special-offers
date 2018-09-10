4f874f5b7cc41d65691166cd60fb6ace9ccc9e4a
Pavel Yanushevsky <4erep.moroz@mail.ru>
2018-09-10 18:32:53 +0300

    Added classes for elements

--- views/special_offer.php
+++ views/special_offer.php
@@ -1,5 +1,5 @@
 <div class="offer-block">
-    <img src="<?php echo DataFilter::sanitizeString($props['offer_image'])?>"
+    <img class="offer-image" src="<?php echo DataFilter::sanitizeString($props['offer_image'])?>"
          alt="<?php echo DataFilter::sanitizeString($props['offer_title'])?>">
-    <p><?php echo DataFilter::sanitizeString($props['offer_title'])?></p>
+    <p class="offer-title"><?php echo DataFilter::sanitizeString($props['offer_title'])?></p>
 </div>
